d82957141a94bddcc6fe42314d58031970807d7d
jessonfoo <jessonfoo@gmail.com>
2016-07-27 09:28:58 -0700

    update git ignore

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,14 +1,28 @@
 class UsersController < ApplicationController
+	before_action authenticate_user!
 	def index
 		if current_user
 			@user = current_user
 		end
 		before_action authenticate_user!
 		redirect_to root_path
-	end
+  end
+
 	def show
+		if current_user
+			@user = current_user
+		end
+		redirect_to root_path
 	end
 	def edit
-	end
+
+  end
+
+  def update
+  end
+
+	def destroy
+  end
+
 end
 
