2a6021efcd837485ed378550a9958c6112f11f28
jessonfoo <jessonfoo@gmail.com>
2016-07-29 11:27:50 -0700

    fix css

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -5,18 +5,12 @@ class PostsController < ApplicationController
       @posts = Post.search(params[:query])
     else
       @posts = Post.find_by_sql "select * from posts where user_id in(select friend_id from friendships where user_id=#{current_user.id})"
-
-  		# @posts = Post.where(user_id: current_user.id)
-      # @posts = Post.joins(:user, :user=>[:friends]).where(friend_id:, current_user.id) 
-      # @posts =  Post.where("user_id IN (?)", current_user.friendships);
-      # @posts = Post.joins(:user, :user=>[:friendships]).where(user_id)
 		end
   end
   
   def new
   	@post = Post.new 
     render 'posts/new'
-    # render json: @post 
   end
   def edit
   	@post = Post.find(params[:id])
@@ -26,7 +20,6 @@ class PostsController < ApplicationController
   def create
     @post= Post.new(post_params)
     @post.user = current_user
-    # @post = current_user.posts.build(post_params)
     if @post.save
       redirect_to posts_path 
     else
@@ -49,12 +42,10 @@ class PostsController < ApplicationController
 
   def update
   	@post = Post.find(params[:id])
-    #assuming params
     if @post.update_attributes(post_params)
     	@post.save
     	redirect_to posts_path
     else
-      #can render differentposts 
       render json: {errors: post.errors}
     end
   end
@@ -71,7 +62,6 @@ class PostsController < ApplicationController
     private
 
   def post_params
-    # params.require(:post).permit!
     params.require(:post).permit(:title,:user_id,:content,:latitude,:longitude,:url)
   end
 end
