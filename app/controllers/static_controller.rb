83ef924786887cf45e7f75e7c9a65d6e9683ed24
jessonfoo <jessonfoo@gmail.com>
2016-09-16 17:36:07 -0700

    try static fix

--- app/controllers/static_controller.rb
+++ app/controllers/static_controller.rb
@@ -4,10 +4,10 @@ class StaticController < ApplicationController
       @posts = Post.search(params[:query])
     else
       @posts = Post.find_by_sql "select * from posts where user_id in(select friend_id from friendships where user_id=#{current_user.id})"
-		end
 			@hash = Gmaps4rails.build_markers(@posts) do |post, marker|
 			  marker.lat post.latitude
 			  marker.lng post.longitude
 			end
+		end
   end
 end
